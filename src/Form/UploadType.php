0135b0abc0708167a64dd3c6b65d0a75cbff2f24
jerome chemusep <7562801-Chemusep@users.noreply.gitlab.comgit>
2021-02-09 13:52:48 +0100

    creation du formulaire dans le HomeHomeController

--- src/Form/UploadType.php
+++ src/Form/UploadType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Upload;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +14,10 @@ class UploadType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', fileType::class, [
+                'label'=> 'Sélectionnez votre fichier',
+            ])
+        ->add('Ajouter', SubmitType::class)
         ;
     }
 
